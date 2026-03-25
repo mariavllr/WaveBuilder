62f7b2423e7557c121d2affea16bc4ca3af0d556
Maria Villar <mariavillardev@gmail.com>
2026-03-25 08:52:44 +0100

    Aserradero, pueblo y screenshots

---
+++ Assets/Scripts/Screenshots.cs
@@ -0,0 +1,31 @@
+using UnityEngine;
+
+using System.IO;
+using UnityEngine;
+
+public class Screenshots : MonoBehaviour
+{
+    string timestamp;
+    string filePath;
+    string folderPath;
+    private void Start()
+    {
+        folderPath = Application.dataPath + "/Screenshots";
+        if (!Directory.Exists(folderPath))
+        {
+            Directory.CreateDirectory(folderPath);
+        }
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.S))
+        {
+            timestamp = System.DateTime.Now.ToString("yyyy-MM-dd_HH-mm-ss");
+            filePath = folderPath + "/WaveBuilderScreenshot_" + timestamp + ".png";
+
+            ScreenCapture.CaptureScreenshot(filePath);
+            Debug.Log(filePath + "saved at Screenshots.");
+        }
+    }
+}
